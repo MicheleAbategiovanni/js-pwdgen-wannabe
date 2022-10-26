f31a68888e90b106c1597d84524f4e2cb04da0bb
MicheleAbategiovanni <michele.abategiovanni@gmail.com>
2022-10-26 15:35:52 +0200

    added script in js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const nomeUtente = prompt('Inserisci il tuo nome:')
+const cognomeUtente = prompt('Inserisci il tuo cognome:')
+const colorePreferito = prompt('Inserisci il tuo colore preferito:')
+
+
+// nome.innerHTML = nomeUtente
+// cognome.innerHTML = cognomeUtente
+
+console.log('Primo progetto con JS')
+console.log('Il tuo nome è:' + ' ' + nomeUtente)
+console.log('Il tuo cognome è:' + ' ' + cognomeUtente)
+console.log('Il tuo colore preferito è:' + ' ' + colorePreferito)
+
+
+
+password.innerHTML = `${nomeUtente}${cognomeUtente}${colorePreferito}21`
